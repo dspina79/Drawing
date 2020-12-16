9693330df581576e3813fd409fd934ec01945538
Dave Spina <dspina79@gmail.com>
2020-12-16 13:09:49 -0500

    Initial project creation

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Dave Spina on 12/16/20.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
